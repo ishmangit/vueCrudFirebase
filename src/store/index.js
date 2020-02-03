6bd3201d1619e574363580704febc582995a4f3c
Isma Baena <ismael.baena@cbre.com>
2020-02-03 16:55:37 +0100

    Pintado de la lista de tareas de firestore

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import db from '@/firebase'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tareas: []
   },
   mutations: {
+    setTareas(state, tareas) {
+      state.tareas = tareas;
+    }
   },
   actions: {
-  },
-  modules: {
+    getTareas({commit}) {
+      const tareas = [];
+      db.collection('tareas').get().then(snapshot => {
+        snapshot.forEach(doc => {
+          let tarea = doc.data();
+          tarea.id = doc.id;
+          tareas.push(tarea);
+        })
+      })
+      commit('setTareas', tareas);
+    }
   }
 })
